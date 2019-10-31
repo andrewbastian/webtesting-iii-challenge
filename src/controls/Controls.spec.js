00c21d8d7eaf5a4c5d47336987e67e2af500c2a3
andrewbastian <andrew.bastian@gmail.com>
2019-10-31 16:18:11 -0700

    MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,30 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Controls from './Controls';
+import { render, fireEvent } from '@testing-library/react';
+
+describe('<Controls />', () => {
+
+    it('matches snapshot', () => {
+      const tree = renderer.create(<Controls />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('buttons toggle closed and locked', () => {
+      const { getByText } = render(<Controls />);
+      getByText (/Lock Gate/);
+      getByText (/Close Gate/);
+    });
+
+    it('closed button disabled if gate locked', () => {
+      const { getByText } = render(<Controls closed={true} locked={true}/>);
+      const openButton = getByText(/Open Gate/);
+      expect(openButton.disabled).toBeTruthy();
+    });
+
+    it('locked button disabled if gate open', () => {
+      const { getByText } = render(<Controls closed={false} locked={false}/>);
+      const lockButton = getByText(/Lock Gate/);
+      expect(lockButton.disabled).toBeTruthy();
+    });
+  });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
-// Test away!
+import React from 'react'
+import { render } from 'react-testing-library'
+import 'react-testing-library/cleanup-after-each';
+import Display from './Display';
+
+describe('<Display />', () => {
+
+  it('state seen (unlocked, open) ', () => {
+    const { getByText } = render(<Display />);
+    expect(getByText('Unlocked'));
+    expect(getByText('Open'));
+  })
+
+  it('state changes through props', () => {
+    const { getByText } = render(<Display locked={true} closed={true} />);
+    expect(getByText('Locked'));
+    expect(getByText('Closed'));
+  })
+})
